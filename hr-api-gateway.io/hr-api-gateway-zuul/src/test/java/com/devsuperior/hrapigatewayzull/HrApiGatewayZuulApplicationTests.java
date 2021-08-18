4878ce6faf5585a5d4a2260569eedbf583c6ec09
Betinsk <gmdust@hotmail.com>
2021-08-18 18:14:12 -0300

    Zull config

---
+++ hr-api-gateway.io/hr-api-gateway-zuul/src/test/java/com/devsuperior/hrapigatewayzull/HrApiGatewayZuulApplicationTests.java
@@ -0,0 +1,15 @@
+package com.devsuperior.hrapigatewayzull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootTest
+class HrApiGatewayZuulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
